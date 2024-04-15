cbc52174e04a1520b9c53faa593fd2481bf2bbe9
khanhan2k2eliai <khanhan.eliai@gmail.com>
2024-04-15 11:59:02 +0700

    use real redis

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,2 +1,2 @@
 import Redis from "ioredis";
-export const redis = new Redis(process.env.REDIS_URI || "");
+export const redis = new Redis("rediss://default:AVNS_p5SxXC8sjRJE8JkNqB9@task-queue-minhnhatdo0709-a715.a.aivencloud.com:17468");
